467c3bec3f374c17deadb4b36af1125b829b7616
Adilonapsh <lubos418@gmail.com>
2023-05-22 22:53:50 +0700

    Portfolio WIP

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,25 @@
 @extends('errors::minimal')
 
+@php
+    $quotes = [
+        "Maaf, Kami sedang mengembangkan aplikasi, mungkin akan bisa dalam beberapa saat.",
+        "Maaf, layanan kami sedang tidak tersedia. Mohon coba lagi nanti.",
+        "Saat ini layanan kami tidak dapat diakses. Silakan coba kembali dalam beberapa saat.",
+        "Layanan sedang sibuk. Mohon tunggu sebentar dan coba lagi.",
+        "Layanan kami sedang mengalami gangguan. Silakan coba kembali nanti.",
+        "Maaf, kami sedang melakukan pemeliharaan pada server. Mohon bersabar dan coba lagi nanti.",
+        "Kami sedang mengalami masalah pada server kami. Mohon kembali lagi dalam beberapa saat.",
+        "Server kami sedang tidak tersedia. Mohon coba kembali dalam beberapa saat.",
+        "Terjadi kesalahan pada server. Mohon kembali lagi dalam beberapa saat.",
+        "Layanan sedang tidak tersedia. Mohon coba lagi nanti atau hubungi tim dukungan kami.",
+        "Maaf, layanan sedang mengalami gangguan. Silakan kembali lagi dalam beberapa saat."
+    ];
+    $picked_quotes = $quotes[rand(0,count($quotes)-1)];
+@endphp
 @section('title', __('Service Unavailable'))
 @section('code', '503')
 @section('message', __('Service Unavailable'))
+@section('images', asset("/assets/icons/image.png"))
+@section('description', $picked_quotes)
+
+
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,228 @@
+<x-guest-layout>
+    <header>
+        <nav class="bg-gradient-to-b fixed w-full z-20 top-0 left-0 from-blue-200 dark:from-[#123873]">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://adilonapsh.com/" class="flex items-center">
+                    {{-- <img src="" class="h-8 mr-3" alt="Flowbite Logo"> --}}
+                    <span class="self-center text-2xl font-black whitespace-nowrap dark:text-white">Adilonapsh</span>
+                </a>
+                <div class="flex md:order-2">
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                        started</button>
+                    <button data-collapse-toggle="navbar-sticky" type="button"
+                        class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-sticky" aria-expanded="false">
+                        <span class="sr-only">Open Sidebar</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 p-2"
+                    id="navbar-sticky">
+                    <ul
+                        class="flex flex-col p-2 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 font-bold text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section
+            class="dark:bg-gray-900 min-h-screen bg-gradient-to-b from-blue-300 from-[10%] to-transparent to-[150%]"
+            style="background-image: url('{{ asset('assets/assets/hero-ornament.png') }}'); background-size:fit;">
+            <div class="flex items-center justify-center h-screen">
+                <div class="flex flex-col order-2 md:order-1">
+                    <h4
+                        class="header-title z-10 text-[2rem] font-bold tracking-tighter mob:text-[2.6rem] md:text-[4rem] text-center md:text-start">
+                        <span data-aos="fade-up" data-aos-delay="100" data-aos-duration="2000">
+                            Adil
+                        </span>
+                        <span data-aos="fade-up" data-aos-delay="300" data-aos-duration="2000">
+                            Ivan .
+                        </span>
+                    </h4>
+                    <h2
+                        class="header-subtitle mt-1 text-[2.2rem] font-bold lowercase leading-[100%] tracking-tight mob:text-[3.4rem] sm:mt-3 sm:text-[4rem] md:mt-1.5 md:text-[5rem] md:leading-[5rem] md:tracking-[-0.06em] lg:max-w-[850px] xl:max-w-[1200px] xl:text-[5.9rem] xl:leading-[6.25rem] text-center md:text-start">
+                        <span data-aos="fade-up" data-aos-delay="400" data-aos-duration="2000">
+                            a
+                            happy full-stack developer </span>
+                        <span data-aos="fade-up" data-aos-delay="600" data-aos-duration="2000">
+                            who aims to help others grow in
+                        </span>
+                        <span data-aos="fade-up" data-aos-delay="800" data-aos-duration="2000">
+                            web development
+                        </span>
+                    </h2>
+                    <p class="mt-10 text-xl xl:text-4xl md:w-7/12">
+                        <span class="font-extrabold">
+                            <span data-aos="fade-up" data-aos-delay="950" data-aos-duration="2000">2</span>
+                            <span data-aos="fade-up" data-aos-delay="1000" data-aos-duration="2000">years </span>
+                            <span data-aos="fade-up" data-aos-delay="950" data-aos-duration="2000">of </span>
+                            <span data-aos="fade-up" data-aos-delay="1050" data-aos-duration="2000">experience</span>
+                        </span>
+                        <span data-aos="fade-up" data-aos-delay="1050" data-aos-duration="2000">in</span>
+                        <span data-aos="fade-up" data-aos-delay="950" data-aos-duration="2000">using</span>
+                        <span data-aos="fade-up" data-aos-delay="1050" data-aos-duration="2000">laravel</span>
+                        <span data-aos="fade-up" data-aos-delay="950" data-aos-duration="2000">and</span>
+                        <span data-aos="fade-up" data-aos-delay="1250" data-aos-duration="2000">node.js</span>
+                        <span data-aos="fade-up" data-aos-delay="950" data-aos-duration="2000">for</span>
+                        <span data-aos="fade-up" data-aos-delay="1350" data-aos-duration="2000">fullstack</span>
+                        <span data-aos="fade-up" data-aos-delay="1250" data-aos-duration="2000">development</span>
+                    </p>
+                </div>
+                {{-- <div class="order-1 md:order-2 relative">
+                    <img src="{{ asset('/assets/assets/hero-ornament.png') }}" alt="" class="absolute right-0">
+                </div> --}}
+            </div>
+        </section>
+        <section class="dark:bg-gray-900 min-h-screen w-full my-5">
+            <div class="flex flex-col items-center mx-auto">
+                <div class="flex items-center">
+                    <img src="{{ asset('/assets/icons/project.png') }}" alt="">
+                    <h4 class="text-[3rem] mob:text-[2.6rem] md:text-[4rem] font-extrabold tracking-tighter capitalize">
+                        Highlighted Projects
+                    </h4>
+                </div>
+                <div class="mt-5">
+                    <article
+                        class="relative z-10 mx-auto max-w-[1244px] overflow-hidden py-20 sm:rounded-3xl bg-[#ac9b2c10] border-[#ac9b2c] border-spacing-x-28 border">
+                        <div class="relative z-10 px-5 sm:px-10 lg:px-16">
+                            <h5 class="text-[2rem] font-bold flex items-center">
+                                <img src="{{ asset('assets/icons/sipbg.png') }}" alt="" width="50">
+                                <span class="ml-2">SIPBG</span>
+                            </h5>
+                            <p class="mt-5 text-[1.2rem]">
+                                <span class="font-bold">Introducing SIPBG</span>
+                                - Building and Building Supervision Information System
+                                (SIPBG) is a website-based application system
+                                developed for convenience
+                                services to the community, especially in
+                                building permit services (IMB) and
+                                Eligible Function (SLF) by the Regional Government.
+                            </p>
+
+                            <div class="ml-10 my-10">
+                                <div class="flex">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-1.png') }}"
+                                        alt="" width="400">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-2.png') }}"
+                                        alt="" width="400">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-1.png') }}"
+                                        alt="" width="400">
+                                </div>
+                            </div>
+
+                            <div class="mt-5">
+                                <h5 class="text-[1.2rem] font-bold">Features :</h5>
+                                <ul class="list-disc pl-5">
+                                    <li>Dashboard</li>
+                                    <li>Web Geographic Information System (GIS)</li>
+                                    <li>Layer Mapping</li>
+                                    <li>Upload SHP File</li>
+                                    <li>Realtime Data</li>
+                                    <li>Integration with OSS (Online Single Submission)</li>
+                                </ul>
+                            </div>
+
+                            <div class="mt-10">
+                                <h5 class="text-[2rem] font-bold">Featured Technology Used</h5>
+                                <div class="flex items-center mt-5">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/laravel.webp') }}"
+                                        alt="" width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/nodejs.webp') }}" alt=""
+                                        width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/postgresql.webp') }}"
+                                        alt="" width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/nginx.webp') }}" alt=""
+                                        width="70" height="70">
+                                </div>
+                            </div>
+                            <img class="absolute top-0 left-0" style="background-size: cover; opacity: .4;"
+                                src="{{ asset('assets/assets/ounament-lines-btm.png') }}" alt="">
+                        </div>
+                    </article>
+                </div>
+                <div class="mt-5">
+                    <article
+                        class="relative z-10 mx-auto max-w-[1244px] overflow-hidden py-20 sm:rounded-3xl bg-[#e8293010] border-[#e82930] border-spacing-x-28 border">
+                        <div class="relative z-10 px-5 sm:px-10 lg:px-16">
+                            <h5 class="text-[2rem] font-bold flex items-center">
+                                <img src="{{ asset('assets/icons/dgis-s.webp') }}" alt="" width="50">
+                                <span class="ml-2">INTERAKSI DGIS</span>
+                            </h5>
+                            <p class="mt-5 text-[1.2rem]">
+                                <span class="font-bold">Introducing Interaksi DGIS</span>
+                                - The innovative drone and GIS applications he initiated can facilitate PUPR's work to
+                                determine topography, land contours, thereby strengthening the need for planning in one
+                                activity.
+                            </p>
+
+                            <div class="ml-10 my-10">
+                                <div class="flex">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-1.png') }}"
+                                        alt="" width="400">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-2.png') }}"
+                                        alt="" width="400">
+                                    <img class="rounded-xl ml-2" src="{{ asset('assets/images/sipbg-1.png') }}"
+                                        alt="" width="400">
+                                </div>
+                            </div>
+
+                            <div class="mt-5">
+                                <h5 class="text-[1.2rem] font-bold">Features :</h5>
+                                <ul class="list-disc pl-5">
+                                    <li>Dashboard</li>
+                                    <li>Web Geographic Information System (GIS)</li>
+                                    <li>Layer Mapping</li>
+                                    <li>Upload SHP File</li>
+                                    <li>Upload 3WS 3D data</li>
+                                    <li>Realtime Data from Simtaru and Sisirah</li>
+                                    <li>Print Map</li>
+                                </ul>
+                            </div>
+
+                            <div class="mt-10 z-10">
+                                <h5 class="text-[2rem] font-bold">Featured Technology Used</h5>
+                                <div class="flex items-center mt-5">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/laravel.webp') }}"
+                                        alt="" width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/nodejs.webp') }}"
+                                        alt="" width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/postgresql.webp') }}"
+                                        alt="" width="70" height="70">
+                                    <img class="ml-5" src="{{ asset('/assets/icons/nginx.webp') }}" alt=""
+                                        width="70" height="70">
+                                </div>
+                            </div>
+
+                            <img class="absolute top-0 right-0 -z-10" style="background-size: contain"
+                                src="{{ asset('assets/assets/ornament-blue.png') }}" alt="">
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+    </main>
+</x-guest-layout>
